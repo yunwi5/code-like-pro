859cfeaf8b66778de25ec3e263f2491237d90730
JD <jdbriones22@gmail.com>
2022-08-24 01:05:46 +1200

    Initialized Header

---
+++ client/src/components/layout/Header.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "client/src/styles/index.scss";
+
+const Header = () => {
+  return <nav className="grey"></nav>;
+};
+
+export default Header;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,24 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{js,jsx,ts,tsx}'],
-    theme: {
-        extend: {
-            colors: {
-                main: {
-                    50: '#e9e9fc',
-                    100: '#bebdf5',
-                    200: '#9391ee',
-                    300: '#6765e7',
-                    400: '#5552E4',
-                    500: '#3c38e0',
-                    600: '#221fc7',
-                    700: '#1b189a',
-                    800: '#13116e',
-                    900: '#0b0a42',
-                    1000: '#040316',
-                },
-            },
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        main: {
+          50: "#e9e9fc",
+          100: "#bebdf5",
+          200: "#9391ee",
+          300: "#6765e7",
+          400: "#5552E4",
+          500: "#3c38e0",
+          600: "#221fc7",
+          700: "#1b189a",
+          800: "#13116e",
+          900: "#0b0a42",
+          1000: "#040316",
         },
+        grey: {
+          500: "#fefefe",
+        },
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 };
