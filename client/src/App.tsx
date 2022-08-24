37c1757c4f36f1db92b41498501c13a40002f394
JD <jdbriones22@gmail.com>
2022-08-25 00:00:11 +1200

    Added routing to Header.tsx, init Login and Register pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,16 +2,22 @@ import { Route, Routes } from "react-router-dom";
 import { AppProperty } from "./constants/app";
 import ExerciseCreation from "./pages/exercise-pages/ExerciseCreation";
 import Home from "./pages/Home";
+import LoginPage from "./pages/auth/LoginPage";
+import RegisterPage from "./pages/auth/RegisterPage";
 import Header from "./components/layout/Header";
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/create-exercise" element={<ExerciseCreation />} />
-      </Routes>
-    </div>
+    <>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create-exercise" element={<ExerciseCreation />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -4,45 +4,41 @@ import styles from "../styles/index.scss";
 const Header = () => {
   return (
     <nav className="flex items-center justify-between flex-wrap px-6 py-1 m-0">
-      <div className="logo flex items-center flex-shrink-0 text-main-500 pl-10">
-        <span className="text-xl tracking-tight">CodeLikePro</span>
+      <div className="logo flex items-center flex-shrink-0 text-main-500 pl-28">
+        <a className="text-xl tracking-tight hover:cursor-pointer">
+          CodeLikePro
+        </a>
       </div>
       <div className="main-font w-full flex flex-grow md:flex md:items-center md:w-auto">
-        <div className="text-sm text-center	md:flex-grow">
+        <div className="text-md text-center	md:flex-grow">
           <a
-            href="#responsive-header"
-            className="block mt-4 lg:inline-block lg:mt-0 text-text-main hover:text-white mr-10"
+            href="/browse"
+            className="block mt-4 lg:inline-block lg:mt-0 text-text-main hover:text-main-700 mr-10"
           >
-            Explore
+            Browse
           </a>
           <a
-            href="#responsive-header"
-            className="block mt-4 lg:inline-block lg:mt-0 text-text-main hover:text-white mr-10"
-          >
-            Problems
-          </a>
-          <a
-            href="#responsive-header"
-            className="block mt-4 lg:inline-block lg:mt-0 text-text-main hover:text-white mr-10"
+            href="/"
+            className="block mt-4 lg:inline-block lg:mt-0 text-text-main hover:text-main-700 mr-10"
           >
             Ranking
           </a>
           <a
-            href="#responsive-header"
-            className="block mt-4 lg:inline-block lg:mt-0 text-text-main hover:text-white"
+            href="/create-exercise"
+            className="block mt-4 lg:inline-block lg:mt-0 text-text-main hover:text-main-700"
           >
             Create
           </a>
         </div>
-        <div className="pr-10">
+        <div className="pr-28">
           <a
-            href="#"
+            href="/login"
             className="text-sm bg-transparent hover:bg-main-500 text-main-700 font-regular hover:text-white py-1 px-4 mr-3 border border-main-500 hover:border-transparent rounded"
           >
             Login
           </a>
           <a
-            href="#"
+            href="/register"
             className="text-sm bg-transparent hover:bg-main-500 text-main-700 font-regular hover:text-white py-1 px-4 border border-main-500 hover:border-transparent rounded"
           >
             Register
