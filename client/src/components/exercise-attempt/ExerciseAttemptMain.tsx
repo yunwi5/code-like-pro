f7e3ac49e0e32e5fafdae2bd32ec9f00afbd4072
Yunkeun <1009jyk@gmail.com>
2022-08-26 05:00:53 +1200

    Add top border to code editor

--- client/src/components/exercise-attempt/ExerciseAttemptMain.tsx
+++ client/src/components/exercise-attempt/ExerciseAttemptMain.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import EditorOutputSection from './editor-output/EditorOutputSection';
-import ExerciseInfo from './exercise-info/ExerciseInfoSection';
+import ExerciseInfoSection from './exercise-info/ExerciseInfoSection';
 
 const ExerciseAttemptMain: React.FC = () => {
     return (
-        <main className="flex gap-5 min-h-[max(90vh,35rem)] max-h-[93vh] bg-gray-200/70">
+        <main className="flex gap-5 min-h-[max(90vh,35rem)] max-h-[93vh] bg-gray-200/70 border-t-2 border-gray-300/90">
             {/* Code editor left side, consisting of prompt, test cases, and scratch pad */}
-            <ExerciseInfo />
+            <ExerciseInfoSection />
             {/* Code editor right side, consisting of control bar, editor and output */}
             <EditorOutputSection />
         </main>
