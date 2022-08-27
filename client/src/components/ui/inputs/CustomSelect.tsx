d6676393ae9767ba015ff3bc19ed84c2550c4999
Yunkeun <1009jyk@gmail.com>
2022-08-27 16:59:58 +1200

    Store mongodb username and password as environment variables

--- client/src/components/ui/inputs/CustomSelect.tsx
+++ client/src/components/ui/inputs/CustomSelect.tsx
@@ -13,7 +13,7 @@ interface Props {
 // App select with custom styles.
 // Reusable select input for consistent styling for the select.
 const CustomSelect: React.FC<Props> = (props) => {
-    const { value, labelText, labelId, options, onChange, id, className } = props;
+    const { value, labelText, labelId, options, onChange, id, className = '' } = props;
 
     const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
         onChange && onChange(e.target.value);
@@ -33,7 +33,7 @@ const CustomSelect: React.FC<Props> = (props) => {
                 className="px-2 py-2 border-2 border-gray-300 bg-gray-50 rounded-sm shadow-md focus:shadow-lg focus:outline focus:outline-2 focus:outline-main-300"
             >
                 {options.map((option, idx) => (
-                    <option className="text-base" key={idx} value={option}>
+                    <option key={idx} value={option}>
                         {option}
                     </option>
                 ))}
