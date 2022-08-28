df63507f86b4178c64b8127eed2c394149bc57ef
DanielMcAnulty <88571133+DanielMcAnulty@users.noreply.github.com>
2022-08-28 17:50:39 +1200

    Fixed issues in exercise controller and switched to Axios to make requests
    Updated testcode to camel case in Exercise model.

--- server/controller/exerciseController.js
+++ server/controller/exerciseController.js
@@ -1,99 +1,113 @@
 const Exercise = require('../models/Exercise');
+const fetch = require('axios');
 
+async function makeRequest(bodyData){
 
-function makeRequest(bodyData){
-    fetch('http://68.183.118.35/jobe/index.php/restapi/runs', {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json; charset-utf-8'
-        },
-        body: JSON.stringify(bodyData),
-    })
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data);
-            return data;
-        });
+    const options = {
+        headers: {'Content-Type': 'application/json; charset-utf-8'}
+    };
+    console.log(bodyData);
 
-}
+    const response = await fetch.post('http://68.183.118.35/jobe/index.php/restapi/runs', JSON.stringify(bodyData), options);
 
+    const data = await response.data;
+    console.log(data);
 
-const postExercise = (req, res) => {
-    
-    const exercise = new Exercise(req.body.exercise);
+    return data;
+
+}  
+
+const postExercise = async (req, res) => {
+    const exerciseBody = req.body;
+    const exercise = new Exercise(exerciseBody);
     exercise.author = req.user._id;
-    
+
     // Check solutionCode works
 
     const testCases = exercise.testCases;
-    const solutionCode = exercise.solutionCode
+    const solutionCode = exercise.solutionCode;
+    let language = exercise.language;
+    // Should need to make sure language_code is received from the client.
 
     // Iterate through each test case and make request to JOBE server
 
-    testCases.forEach(testCase => {
-
+    const testCasePromises = testCases.map((testCase) => {
         // Append test case to solution code and check if output is right
 
-        const test = solutionCode + "\n" + testCase.testcode;
+        const test = solutionCode + "\n" + testCase.testCode;
 
-        const body =  {
-            run_spec: {language_id: testCase.language_id, sourcefilename: "test", sourcecode:test}};
+        const body = {
+            run_spec: {
+                language_id: language,
+                sourcefilename: 'test',
+                sourcecode: test,
+            },
+        };
 
-        const result = makeRequest(body);
-
-        if (result.stdout != testCase.expectedOutput){
-            return res.status(400).json(result);
-        }
-
+        const result =  makeRequest(body);
+        return result;
     });
+
+    const testCaseResults = await Promise.all(testCasePromises);
+
+    console.log(testCaseResults);
+    for (let i = 0; i < testCaseResults.length; i++) {
+        if (testCaseResults[i].stdout.trim() != testCases[i].expectedOutput.trim()) {
+            return res.status(400).json({ message: 'Some test cases failed.' });
+        }
+    }
 
     await exercise.save();
     console.log('new exercise', exercise);
 
-    res.json(exercise);
-    
-}
+    res.status(201).json(exercise);
+};
 
-const getExercises = (req, res) => {
+
+const getExercises = async (req, res) => {
     // populate author field with author name
     const exercises = await Exercise.find({}).populate('author', 'name');
     res.status(200).json(exercises);
-}
+};
 
-const getExerciseByID = (req, res) => {
+const getExerciseByID = async (req, res) => {
     // populate author field with author name
     const exercise = await Exercise.findById(req.params.id).populate('author', 'name');
     res.status(200).json(exercise);
-}
+};
 
-const updateExercise = (req, res) => {
+const updateExercise = async (req, res) => {
     // get new exercise and test solution code
     const newExercise = new Exercise(req.body.exercise);
     const testCases = newExercise.testCases;
     const solutionCode = newExercise.solutionCode;
 
-    testCases.forEach(testCase => {
-        const test = solutionCode + "\n" + testCase.testcode;
+    testCases.forEach((testCase) => {
+        const test = solutionCode + '\n' + testCase.testcode;
 
-        const body =  {
-            run_spec: {language_id: testCase.language_id, sourcefilename: "test", sourcecode:test}};
+        const body = {
+            run_spec: {
+                language_id: testCase.language_id,
+                sourcefilename: 'test',
+                sourcecode: test,
+            },
+        };
 
         const result = makeRequest(body);
 
-        if (result.stdout != testCase.expectedOutput){
+        if (result.stdout != testCase.expectedOutput) {
             return res.status(400).json(result);
         }
     });
 
     const updatedExercise = await Exercise.findByIdAndUpdate(req.params.id, newExercise);
     res.status(201).json(updatedExercise);
-}
+};
 
-const deleteExercise = (req, res) => {
+const deleteExercise = async (req, res) => {
     await Exercise.findByIdAndRemove(req.params.id);
     res.status(204);
-}
-
+};
 
 const controller = {
     postExercise,
--- server/models/Exercise.js
+++ server/models/Exercise.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const TestCaseSchema = new Schema({
-   testcode: {
+   testCode: {
         type: String,
         required: true,
    },
