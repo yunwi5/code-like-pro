5a9f71b2fc3b0fc08ef95668a302ed8255c4b0ae
DanielMcAnulty <88571133+DanielMcAnulty@users.noreply.github.com>
2022-09-05 19:50:43 +1200

    Fixes to getUserByID

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,19 +1,24 @@
 const UserSubmission = require('../models/UserSubmission');
 const User = require('../models/User');
+const Exercise = require('../models/Exercise');
 
 
 const getUserByID = async (req, res) => {
     let user;
     try {
         // populate author field with author name
-        user = await User.findById(req.params.id).populate('liked').populate('exercises');
+        user = await User.findById(req.params.id).populate('liked');
     } catch (err) {
         console.log(err.message);
     }
 
     if (user != null) {
         const userSubmissions = await UserSubmission.find({user: req.params.id}).populate('exercise');
+        const userExercises = await Exercise.find({author: req.params.id});
+
         user.submissions = userSubmissions;
+        user.exercises = userExercises;
+        
 
         res.status(200).json(user);
     } else {
