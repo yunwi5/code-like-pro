6a9bc165dec1adc4884cbe49fdc655770c4784d3
DanielMcAnulty <88571133+DanielMcAnulty@users.noreply.github.com>
2022-09-14 17:48:02 +1200

    ShowCase APIs
    Testing still required

---
+++ server/routes/showCase.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const showCaseController = require('../controller/showCaseController');
+const { ensureAuthenticated } = require('../middleware/auth');
+const catchAsync = require('../middleware/catchAsync');
+
+const router = express.Router();
+
+// Get show case
+router.get('/:id', ensureAuthenticated, catchAsync(showCaseController.getShowCaseByID));
+
+// Get all show cases
+router.get('/', ensureAuthenticated, catchAsync(showCaseController.getShowCase));
+
+// Delete show case
+router.delete('/:id', ensureAuthenticated, catchAsync(showCaseController.deleteShowCase));
+
+// Create show case
+router.post('/', ensureAuthenticated, catchAsync(showCaseController.postShowCase));
+
+// Edit show case
+router.patch('/', ensureAuthenticated, catchAsync(showCaseController.updateShowCase));
+
+module.exports = router;
