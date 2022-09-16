658149b99b5988f7aea5c38bde0906b2a331890c
JD <jdbriones22@gmail.com>
2022-09-16 22:08:34 +1200

    Added Sorter and added placeholder value for number of showcases

---
+++ client/src/components/showcase/showcases/ShowcaseShowcases.tsx
@@ -0,0 +1,32 @@
+import React, { useMemo, useState } from "react";
+import { useShowcase } from "../../../store/context/ShowcaseContext";
+import CodeEditor from "../../ui/editor/CodeEditor";
+import { CommentSortingKey, SortingDirection } from "../../../models/enums";
+import ShowcaseSorter from "./sections/ShowcaseSorter";
+
+const ShowcaseShowcases: React.FC = () => {
+  const { userSubmission } = useShowcase();
+  if (!userSubmission) return null;
+
+  const [sortingState, setSortingState] = useState({
+    key: CommentSortingKey.NONE,
+    direction: SortingDirection.DESCENDING,
+  });
+
+  return (
+    <div>
+      <div className="flex flex-row">
+        <ShowcaseSorter
+          sortingState={sortingState}
+          setSortingState={setSortingState}
+        />
+        <h5 className="text-gray-500 font-bold text-lg ml-auto">
+          {5} Showcases
+        </h5>
+      </div>
+      {/* Component that handles the selection of sorting key and direction from the user. */}
+    </div>
+  );
+};
+
+export default ShowcaseShowcases;
--- client/src/models/enums/sorting/CommentSortingKey.ts
+++ client/src/models/enums/sorting/CommentSortingKey.ts
@@ -1,7 +1,9 @@
 export enum CommentSortingKey {
-    NONE = 'None',
-    DATETIME = 'Date & time', // Sorting by datetime of the comment post.
-    VOTES = 'Votes', // Sorting by upvotes - downvotes value
+  NONE = "None",
+  DATETIME = "Date & time", // Sorting by datetime of the comment post.
+  VOTES = "Votes", // Sorting by upvotes - downvotes value
 }
 
-export const CommentSortingKeyList = Object.freeze(Object.values(CommentSortingKey));
+export const CommentSortingKeyList = Object.freeze(
+  Object.values(CommentSortingKey)
+);
