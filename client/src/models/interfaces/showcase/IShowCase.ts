6fd8a24bb2f4b888d2823e31007308859ad1642c
JD <jdbriones22@gmail.com>
2022-09-18 14:52:01 +1200

    Showcases basic implementation, need to add functionality

--- client/src/models/interfaces/showcase/IShowCase.ts
+++ client/src/models/interfaces/showcase/IShowCase.ts
@@ -1,12 +1,12 @@
-import { IVote } from '..';
+import { IVote } from "..";
 
 // Showcase unpopulated version
 export interface IShowCase {
-    _id: string;
-    code: string;
-    description: string;
-    postedAt: string; // ISO date
-    user: string; // User id (user is not populated)
-    comments: string[]; // list of comment ids (comments are not populated)
-    votes: IVote[];
+  _id: string;
+  code: string;
+  description: string;
+  postedAt: string; // ISO date
+  user: string; // User id (user is not populated)
+  comments: string[]; // list of comment ids (comments are not populated)
+  votes: IVote[];
 }
