9ea7a3c73ffc6e06af8742382c1599bdbcaf63c2
Amotys <amotyslee@gmail.com>
2022-09-30 19:50:24 +1300

    Map function in podium

--- client/src/components/ranking/RankingPodium.tsx
+++ client/src/components/ranking/RankingPodium.tsx
@@ -8,6 +8,8 @@ interface Props {
 
 const RankingPodium: React.FC<Props> = (props) => {
     let rankingArray = props.rankingArray;
+    let order = [1,0,2];
+    let colors = ['#3c38e0','#6a38e0','#9238e0']
 
     return (
         <>
@@ -18,57 +20,41 @@ const RankingPodium: React.FC<Props> = (props) => {
                         className="flex flex-nowrap items-end"
                         style={{ wordBreak: 'keep-all', wordWrap: 'break-word' }}
                     >
-                        {(() => {
-                            let indexOrder = [1, 0, 2];
-                            let td = [];
-                            let color = '';
-                            for (let index of indexOrder) {
-                                if (index < rankingArray.length) {
-                                    if (index == 0) {
-                                        color = '#3c38e0';
-                                    }
-                                    if (index == 1) {
-                                        color = '#6a38e0';
-                                    }
-                                    if (index == 2) {
-                                        color = '#9238e0';
-                                    }
-                                    td.push(
-                                        <td
-                                            key={'RankingPodiumTd' + index.toString()}
-                                            style={{
-                                                wordBreak: 'keep-all',
-                                                wordWrap: 'break-word',
-                                            }}
-                                            className="text-center text-gray-600 font-bold px-8 inline-block align-bottom"
-                                        >
-                                            <img
-                                                src={
-                                                    rankingArray[index].pictureUrl ||
-                                                    default_profile
-                                                }
-                                                className="rounded-full h-20 w-20 inline-block"
-                                            />
-                                            <div className="py-5">
-                                                {rankingArray[index].name}
-                                            </div>
-                                            <div
-                                                style={{
-                                                    height: 100 - index * 25,
-                                                    color: color,
-                                                }}
-                                                className={
-                                                    'w-48 text-4xl flex flex-row items-center rounded-lg justify-center text-gray-600 bg-gray-100 shadow-lg'
-                                                }
-                                            >
-                                                {index + 1}
-                                            </div>
-                                        </td>,
-                                    );
-                                }
-                            }
-                            return td;
-                        })()}
+                        {order.map((index)=>(
+                            (index < rankingArray.length)?
+                                <td
+                                    key={'RankingPodiumTd' + index.toString()}
+                                    style={{
+                                        wordBreak: 'keep-all',
+                                        wordWrap: 'break-word',
+                                    }}
+                                    className="text-center text-gray-600 font-bold px-8 inline-block align-bottom"
+                                >
+                                    <img
+                                        src={
+                                            rankingArray[index].pictureUrl ||
+                                            default_profile
+                                        }
+                                        className="rounded-full h-20 w-20 inline-block"
+                                    />
+                                    <div className="py-5">
+                                        {rankingArray[index].name}
+                                    </div>
+                                    <div
+                                        style={{
+                                            height: 100 - index * 25,
+                                            color: (index != 0)? (index != 1)? colors[2]: colors[1]: colors[0],
+                                            minWidth: 180,
+                                        }}
+                                        className={
+                                            'text-4xl flex flex-row items-center rounded-lg justify-center text-gray-600 bg-gray-100 shadow-lg'
+                                        }
+                                    >
+                                        {index + 1}
+                                    </div>
+                                </td>
+                            :''
+                        ))}
                     </tr>
                 </tbody>
             </table>
