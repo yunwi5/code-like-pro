c77f2b44ca07aa0c19b8a1a544e1f91a6db24d67
Yunkeun <1009jyk@gmail.com>
2022-10-01 20:33:28 +1300

    Add ranking functionality to the profile page

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,47 +1,46 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Helmet } from "react-helmet";
-import { motion } from "framer-motion";
-import { AppProperty } from "../constants/app";
-import HeroLanding from "../components/home/HeroLanding";
-import Carousel from "../components/home/Carousel";
-import TopExercises from "../components/home/TopExercises";
-import TopUsers from "../components/home/TopUsers";
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import { motion } from 'framer-motion';
+import { AppProperty } from '../constants/app';
+import HeroLanding from '../components/home/HeroLanding';
+import Carousel from '../components/home/Carousel';
+import TopExercises from '../components/home/TopExercises';
+import TopUsers from '../components/home/TopUsers';
 
 const Home: React.FC = () => {
-  return (
-    <>
-      <Helmet>
-        <title>Home | {AppProperty.APP_NAME}</title>
-        <meta
-          name="description"
-          content={`Home page of ${AppProperty.APP_NAME} where users can see detailed information about programming exercises on the website.`}
-        />
-      </Helmet>
-      <HeroLanding />
-      <motion.div
-        initial={{ opacity: 0, y: 100 }}
-        whileInView={{ opacity: 1, y: 0, transition: { duration: 0.8 } }}
-        viewport={{ once: true }}
-      >
-        <Carousel />
-      </motion.div>
-      <motion.div
-        initial={{ opacity: 0, x: -300 }}
-        whileInView={{ opacity: 1, x: 0, transition: { duration: 1 } }}
-        viewport={{ once: true }}
-      >
-        <TopExercises />
-      </motion.div>
-      <motion.div
-        initial={{ opacity: 0, x: 300 }}
-        whileInView={{ opacity: 1, x: 0, transition: { duration: 1 } }}
-        viewport={{ once: true }}
-      >
-        <TopUsers />
-      </motion.div>
-    </>
-  );
+    return (
+        <>
+            <Helmet>
+                <title>Home | {AppProperty.APP_NAME}</title>
+                <meta
+                    name="description"
+                    content={`Home page of ${AppProperty.APP_NAME} where users can see detailed information about programming exercises on the website.`}
+                />
+            </Helmet>
+            <HeroLanding />
+            <motion.div
+                initial={{ opacity: 0, y: 100 }}
+                whileInView={{ opacity: 1, y: 0, transition: { duration: 0.8 } }}
+                viewport={{ once: true }}
+            >
+                <Carousel />
+            </motion.div>
+            <motion.div
+                initial={{ opacity: 0, x: -300 }}
+                whileInView={{ opacity: 1, x: 0, transition: { duration: 1 } }}
+                viewport={{ once: true }}
+            >
+                <TopExercises />
+            </motion.div>
+            <motion.div
+                initial={{ opacity: 0, x: 300 }}
+                whileInView={{ opacity: 1, x: 0, transition: { duration: 1 } }}
+                viewport={{ once: true }}
+            >
+                <TopUsers />
+            </motion.div>
+        </>
+    );
 };
 
 export default Home;
