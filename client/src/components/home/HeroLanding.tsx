d06c56d80bc98c0b828fffbde3ecfbba37b86d5b
JD <jdbriones22@gmail.com>
2022-10-11 17:31:13 +1300

    cursor and animation disappear for smaller screens

--- client/src/components/home/HeroLanding.tsx
+++ client/src/components/home/HeroLanding.tsx
@@ -5,21 +5,24 @@ import '../../styles/typewriter.css';
 import { Typewriter } from './TypeWriter';
 
 const HeroLanding: React.FC = () => {
-    const typerRef = useRef<HTMLHeadingElement>(null);
+    // const typerRef = useRef<HTMLHeadingElement>(null);
 
-    useEffect(() => {
-        if (!typerRef.current) return;
-        const typeWriter = new Typewriter(typerRef.current, {
-            loop: false,
-            typingSpeed: 60,
-        });
-        typeWriter.typeString('Programming exercises created for students by students');
-        typeWriter.start();
-    }, []);
+    // useEffect(() => {
+    //     if (!typerRef.current) return;
+    //     const typeWriter = new Typewriter(typerRef.current, {
+    //         loop: false,
+    //         typingSpeed: 60,
+    //     });
+    //     typeWriter.typeString('Programming exercises created for students by students');
+    //     typeWriter.start();
+    // }, []);
     return (
         <div className="w-full flex-center flex-col text-center overflow-hidden h-[95vh]">
-            <div className="flex">
-                <h1 className="text-5xl" ref={typerRef}></h1>
+            <div className="typewriter">
+                {/* <h1 className="text-5xl" ref={typerRef}></h1> */}
+                <h1 className="text-4xl">
+                    Programming exercises created for students by students
+                </h1>
             </div>
 
             <h2 className="m-6 lg:text-xl ">
