e9cb44864fcc14d4d013e57fa9c2d332b3d54d77
Yunkeun <1009jyk@gmail.com>
2022-10-16 04:31:48 +1300

    Set up client ready for deployment

--- client/src/constants/app.ts
+++ client/src/constants/app.ts
@@ -1,5 +1,4 @@
 export const AppProperty = {
     APP_NAME: 'CodeLikePro',
-    // SERVER_DOMAIN: 'https://code-like-pro-production.up.railway.app',
-    SERVER_DOMAIN: 'http://localhost:8080',
+    SERVER_DOMAIN: 'https://code-like-pro-production.up.railway.app',
 };
