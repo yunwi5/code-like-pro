3215fb3715d202804d7c9aebbb7d37bc5cac753b
Yunkeun <1009jyk@gmail.com>
2022-10-20 09:28:38 +1300

    Adjust browsing page styling

--- client/src/components/browsing/sidebar/BrowsingSidebar.tsx
+++ client/src/components/browsing/sidebar/BrowsingSidebar.tsx
@@ -11,16 +11,19 @@ import TagsFilter from './sections/TagsFilter';
 interface Props {
     exercises: IExerciseCard[];
     onShuffle: (shuffled: IExerciseCard[]) => void;
+    className?: string;
 }
 
-const BrowsingSidebar: React.FC<Props> = ({ exercises, onShuffle }) => {
+const BrowsingSidebar: React.FC<Props> = ({ exercises, onShuffle, className }) => {
     const shuffleExercises = () => {
         const shuffled = shuffleList(exercises);
         onShuffle(shuffled);
     };
 
     return (
-        <aside className="lg:sticky top-16 flex flex-col gap-4 lg:basis-1/3 px-3 py-4 text-gray-700 bg-gray-200/90 rounded-sm shadow-md">
+        <aside
+            className={`lg:sticky top-16 flex flex-col gap-4 lg:basis-1/3 px-3 py-4 text-gray-700 bg-gray-200/90 rounded-sm shadow-md ${className}`}
+        >
             <div className="-mb-2 flex-between">
                 <h3 className="text-lg">Find Your Best Fit</h3>
                 <HoveringLabel
--- client/src/components/ui/inputs/CustomSelect.tsx
+++ client/src/components/ui/inputs/CustomSelect.tsx
@@ -7,9 +7,10 @@ interface Props {
     options: number[] | string[] | readonly string[];
     // option labels that represent human readable format of the options (optional)
     optionLabels?: string[] | JSX.Element[];
-    className?: string;
     onChange?: (value: string) => void;
     value?: string;
+    className?: string;
+    selectClassName?: string;
 }
 
 // App select with custom styles.
@@ -23,6 +24,7 @@ const CustomSelect: React.FC<Props> = (props) => {
         id,
         optionLabels = [],
         className = '',
+        selectClassName = '',
     } = props;
 
     const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
@@ -36,7 +38,7 @@ const CustomSelect: React.FC<Props> = (props) => {
                 id={id}
                 value={value}
                 onChange={handleChange}
-                className="px-2 py-2 border-2 border-gray-300 bg-gray-50 rounded-sm shadow-md focus:bg-white focus:shadow-lg focus:outline focus:outline-2 focus:outline-main-300"
+                className={`input ${selectClassName}`}
             >
                 {options.map((option, idx) => (
                     <option key={idx} value={option}>
