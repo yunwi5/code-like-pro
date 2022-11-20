cc86f96c91ba38188e1908ea8b75b2190ded24c2
Yunkeun <1009jyk@gmail.com>
2022-11-20 15:48:08 +1300

    Fix add test-case btn tooltip text

--- client/src/components/exercise-attempt/exercise-info/sections/test-cases/TestCaseUserActions.tsx
+++ client/src/components/exercise-attempt/exercise-info/sections/test-cases/TestCaseUserActions.tsx
@@ -9,7 +9,7 @@ interface Props {
 const TestCaseUserActions: React.FC<Props> = ({ onAddCase }) => {
     return (
         <div className="pr-4">
-            <HoveringLabel label="Add Case" className="!text-sm" onClick={onAddCase}>
+            <HoveringLabel label="Add" className="!text-sm" onClick={onAddCase}>
                 <button className="px-1 py-1 text-2xl rounded-md shadow bg-slate-200 text-slate-600 hover:bg-slate-600 hover:text-white">
                     <HiPlus />
                 </button>
