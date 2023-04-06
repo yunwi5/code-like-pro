f339e74f2a26f7db92e35ad7236431dab7ed5530
yunwi5 <1009jyk@gmail.com>
2023-04-07 01:05:25 +1200

    simple fix

--- client/components/forum/post-detail/sections/PostShare.tsx
+++ client/components/forum/post-detail/sections/PostShare.tsx
@@ -8,47 +8,47 @@ import HoveringLabel from '../../../ui/tooltip/HoveringLabel';
 import SocialPanel from '../../../ui/social/SocialPanel';
 
 interface Props {
-    post: IForumPostPopulated;
-    className?: string;
+  post: IForumPostPopulated;
+  className?: string;
 }
 
 const PostShare: React.FC<Props> = ({ post, className = '' }) => {
-    const [showPanel, setShowPanel] = useState<boolean>(false);
-    const shareUrl = `${AppProperty.CLIENT_DOMAIN}${getForumPostLink(post)}`;
+  const [showPanel, setShowPanel] = useState<boolean>(false);
+  const shareUrl = `${window.location.hostname}${getForumPostLink(post)}`;
 
-    return (
-        <div className={`flex-center relative ${className}`}>
-            <HoveringLabel label="Share!" className="">
-                <button onClick={() => setShowPanel((ps) => !ps)}>
-                    <div
-                        className={`px-2 py-2 bg-slate-200 hover:text-slate-50 hover:bg-slate-500 rounded shadow hover:shadow-md transition-all`}
-                    >
-                        <BsShare className="m-1 mr-2" />
-                    </div>
-                </button>
-            </HoveringLabel>
+  return (
+    <div className={`flex-center relative ${className}`}>
+      <HoveringLabel label="Share!" className="">
+        <button onClick={() => setShowPanel((ps) => !ps)}>
+          <div
+            className={`px-2 py-2 bg-slate-200 hover:text-slate-50 hover:bg-slate-500 rounded shadow hover:shadow-md transition-all`}
+          >
+            <BsShare className="m-1 mr-2" />
+          </div>
+        </button>
+      </HoveringLabel>
 
-            <AnimatePresence>
-                {showPanel && (
-                    <motion.div
-                        initial={{ opacity: 0, y: 0, x: -135 }}
-                        animate={{ opacity: 1, y: -35, x: -135 }}
-                        exit={{ opacity: 0, y: 0, x: -135 }}
-                    >
-                        <SocialPanel
-                            onClose={() => setShowPanel(false)}
-                            url={shareUrl}
-                            className="bottom-[120%]"
-                            title={`${post.name} - CodeLikePro`}
-                            tags={[...(post.tags || []), 'Showcase']}
-                            source={post.content}
-                            via={AppProperty.APP_NAME}
-                        />
-                    </motion.div>
-                )}
-            </AnimatePresence>
-        </div>
-    );
+      <AnimatePresence>
+        {showPanel && (
+          <motion.div
+            initial={{ opacity: 0, y: 0, x: -135 }}
+            animate={{ opacity: 1, y: -35, x: -135 }}
+            exit={{ opacity: 0, y: 0, x: -135 }}
+          >
+            <SocialPanel
+              onClose={() => setShowPanel(false)}
+              url={shareUrl}
+              className="bottom-[120%]"
+              title={`${post.name} - CodeLikePro`}
+              tags={[...(post.tags || []), 'Showcase']}
+              source={post.content}
+              via={AppProperty.APP_NAME}
+            />
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
+  );
 };
 
 export default PostShare;
--- client/constants/app.ts
+++ client/constants/app.ts
@@ -1,11 +1,6 @@
 export const AppProperty = {
     APP_NAME: 'CodeLikePro',
-    SERVER_DOMAIN:
-        process.env.NODE_ENV === 'production'
-            ? 'https://code-like-pro-production.up.railway.app'
-            : 'http://localhost:8080',
-
-    CLIENT_DOMAIN: 'https://code-like-pro.vercel.app',
+    SERVER_DOMAIN: 'https://code-like-pro-production.up.railway.app',
     GOOGLE_CLIENT_ID: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID ?? '',
 };
 
