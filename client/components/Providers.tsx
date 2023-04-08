992adb115dd34ab49d622224945ea332fdceae1e
yunwi5 <1009jyk@gmail.com>
2023-04-08 22:23:03 +1200

    update providers

--- client/components/Providers.tsx
+++ client/components/Providers.tsx
@@ -1,10 +1,12 @@
 'use client';
-import { AppProperty } from '@/constants/app';
+import { AppProperty } from '@/constants';
+import { UserContextProvider } from '@/store/context/UserContext';
 import store from '@/store/redux/store';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
 
 const queryClient = new QueryClient();
 
@@ -12,8 +14,11 @@ const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <Provider store={store}>
       <GoogleOAuthProvider clientId={AppProperty.GOOGLE_CLIENT_ID}>
-        <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+        <QueryClientProvider client={queryClient}>
+          <UserContextProvider>{children}</UserContextProvider>
+        </QueryClientProvider>
       </GoogleOAuthProvider>
+      <ToastContainer />
     </Provider>
   );
 };
