27e7f44cefd719a1f24253ea590b5796a544b982
Yunkeun Jo <yunkeun.jo@halter.co.nz>
2023-04-09 23:24:07 +1200

    rename page components

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -8,7 +8,7 @@ export const metadata = {
   description: `Login page of ${AppProperty.APP_NAME} where users can log into their accounts.`,
 };
 
-const page: NextPage = () => {
+const LoginPage: NextPage = () => {
   return (
     <main className="flex-center lg:gap-8 xl:gap-[7.5%] mb-6 min-h-[max(85vh,35rem)] px-3 md:px-5 xl:px-10 py-16 text-gray-700">
       <LoginForm />
@@ -16,4 +16,4 @@ const page: NextPage = () => {
   );
 };
 
-export default page;
+export default LoginPage;
--- client/app/register/page.tsx
+++ client/app/register/page.tsx
@@ -8,7 +8,7 @@ export const metadata = {
   description: `Registration page of ${AppProperty.APP_NAME} where users can create a new account.`,
 };
 
-const page: NextPage = () => {
+const RegisterPage: NextPage = () => {
   return (
     <main className="flex-center min-h-[max(85vh,35rem)] px-3 md:px-5 xl:px-10 py-10 text-gray-700">
       <RegisterForm />
@@ -16,4 +16,4 @@ const page: NextPage = () => {
   );
 };
 
-export default page;
+export default RegisterPage;
