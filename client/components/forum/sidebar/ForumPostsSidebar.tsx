a2c0b63f4849c284aca33ff9f80ecb91a7c8a9ae
Yunkeun Jo <yunkeun.jo@halter.co.nz>
2023-04-11 23:36:10 +1200

    fix max height

--- client/components/forum/sidebar/ForumPostsSidebar.tsx
+++ client/components/forum/sidebar/ForumPostsSidebar.tsx
@@ -5,7 +5,7 @@ import ForumPostsList from './sections/ForumPostsList';
 const ForumPostsSidebar: React.FC<{ className?: string }> = ({ className = '' }) => {
   return (
     <aside
-      className={`relative card w-full lg:w-fit max-h-[84vh] flex flex-col min-w-[min(22.5rem,90vw)] bg-slate-100 rounded ${className}`}
+      className={`relative card w-full lg:w-fit max-h-[85vh] flex flex-col min-w-[min(22.5rem,90vw)] bg-slate-100 rounded ${className}`}
     >
       <section className="sticky top-[3.5rem] h-full flex flex-col">
         <ForumPostsControl />
