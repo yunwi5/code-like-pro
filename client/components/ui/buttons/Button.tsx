ee25aefe45efc3a27d645cde3f4bfcf508055ea4
Yunkeun Jo <yunkeun.jo@halter.co.nz>
2023-04-13 20:47:18 +1200

    consolidate 404 page

--- client/components/ui/buttons/Button.tsx
+++ client/components/ui/buttons/Button.tsx
@@ -7,19 +7,28 @@ interface Props {
   onClick?: (e: React.MouseEvent) => void;
   mode?: 'fill' | 'empty';
   size?: 'small' | 'medium' | 'large';
+  theme?: 'main' | 'indigo' | 'purple' | 'pink';
 }
 
 // Re-usable button component for our app theme.
 // btn class and btn-fill & btn-empty classes are defined in the index.scss file.
 const Button: React.FC<Props> = (props) => {
-  const { className, children, onClick, type, mode = 'fill', size = 'medium' } = props;
+  const {
+    className,
+    children,
+    onClick,
+    type,
+    mode = 'fill',
+    size = 'medium',
+    theme = 'main',
+  } = props;
   const modeClass = mode === 'fill' ? 'btn-fill' : 'btn-empty';
   const sizeClass = size === 'small' ? 'btn-small' : size === 'large' ? 'btn-large' : '';
 
   return (
     <button
       type={type}
-      className={`btn ${modeClass} ${sizeClass} ${className ?? ''}`}
+      className={`btn ${modeClass} ${sizeClass} ${theme} ${className ?? ''}`}
       onClick={onClick}
     >
       {children}
--- client/components/utils/Error404.tsx
+++ client/components/utils/Error404.tsx
@@ -1,27 +1,47 @@
-import React from 'react';
+import React, { FC } from 'react';
+import { BiHome } from 'react-icons/bi';
+import { IoNavigate } from 'react-icons/io5';
+import { TbError404 } from 'react-icons/tb';
+import Link from 'next/link';
+
+import Button from '../ui/buttons/Button';
+
+type LinkInfo = { href: string; text: string | JSX.Element };
 
 interface Error404Props {
   message?: string;
-  homePath?: string;
+  additionalLink?: LinkInfo;
 }
 
-const Error404: React.FC<Error404Props> = ({ message = 'Page Not Found', homePath = '/' }) => {
+const styles = {
+  linkButton: 'w-[90%] sm:w-[13.5rem]',
+  linkIcon: 'inline-block mr-1 text-[1.5em]',
+};
+
+const Error404: FC<Error404Props> = ({ message = 'Page Not Found', additionalLink }) => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen text-center">
-      <div className="space-y-4">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-          className="w-24 h-24 text-red-500"
-        >
-          <path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z" />
-        </svg>
-        <div className="text-6xl font-bold">404</div>
-        <div className="text-2xl">{message}</div>
-        <a href={homePath} className="mt-4 text-xl text-blue-600 hover:text-blue-800">
-          Go Back to Home
-        </a>
+    <div className="flex flex-col items-center justify-center min-h-[85vh] min-w-[50vw] text-center">
+      <div className="flex-center flex-col gap-4">
+        <TbError404 className="text-main-500 text-[25rem]" />
+        <div className="-mt-[3rem] flex-center gap-2 text-3xl font-bold text-gray-600">
+          {message}
+        </div>
+
+        <div className="mt-5 flex items-center flex-col sm:flex-row gap-4 text-xl capitalize">
+          <Link href="/">
+            <Button theme="indigo" mode="empty" className={styles.linkButton}>
+              <BiHome className={styles.linkIcon} /> Back to Home
+            </Button>
+          </Link>
+          {additionalLink && (
+            <Link href={additionalLink.href}>
+              <Button theme="pink" mode="empty" className={styles.linkButton}>
+                <IoNavigate className={styles.linkIcon} />
+                {additionalLink.text}
+              </Button>
+            </Link>
+          )}
+        </div>
       </div>
     </div>
   );
