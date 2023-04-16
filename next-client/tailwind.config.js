418dde6e3468dd2f35db2c407bee4e16bd6789b7
Yunkeun Jo <yunkeun.jo@halter.co.nz>
2023-04-16 22:21:51 +1200

    fix tailwind config

--- next-client/tailwind.config.js
+++ next-client/tailwind.config.js
@@ -2,10 +2,8 @@
 module.exports = {
   content: [
     './app/**/*.{js,ts,jsx,tsx}', // Note the addition of the `app` directory.
-    './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
-    // Or if using `src` directory:
-    './src/**/*.{js,ts,jsx,tsx}',
+    './utils/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
