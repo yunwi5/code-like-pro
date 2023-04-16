a3702184a7677cfc8623c1b10378a90692865c08
Yunkeun Jo <yunkeun.jo@halter.co.nz>
2023-04-17 00:08:51 +1200

    setup docker compose

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,14 +1,5 @@
-const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
-const MONGO_USERNAME = process.env.MONGO_USERNAME;
-const DB_NAME = 'code-like-pro';
-const MongodbURI = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.7z3ly69.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
-
 const keys = {
-    // Connect to the cloud database if the password env variale exists, otherwise connect to local mongodb.
-    MongoURI:
-        MONGO_USERNAME && MONGO_PASSWORD
-            ? MongodbURI
-            : 'mongodb://localhost:27017/code-like-pro',
+    MongoURI: process.env.MONGO_URI,
     ClientBaseURL:
         process.env.NODE_ENV === 'production'
             ? 'https://code-like-pro.vercel.app'
