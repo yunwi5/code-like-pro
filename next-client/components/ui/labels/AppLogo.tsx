2ef2ab6084c57c4580ac6236cd7bfd11d2d18923
yunwi5 <1009jyk@gmail.com>
2023-04-25 16:05:35 +1200

    revert "clean up styling"
    This reverts commit f8b21bf9c30d2d4430403e9b2a0a883632abdb49.

--- next-client/components/ui/labels/AppLogo.tsx
+++ next-client/components/ui/labels/AppLogo.tsx
@@ -16,7 +16,7 @@ const AppLogo: FC<AppLogoProps> = ({ className, onClick, logoSize = 25 }) => {
     <Link
       href="/"
       onClick={onClick}
-      className={`flex-start gap-1 sm:gap-2 text-main-600 font-normal tracking-tight ${redHatMono.className} ${className}`}
+      className={`flex-start gap-1 sm:gap-2 text-main-600 font-normal tracking-tight cursor-pointer ${redHatMono.className} ${className}`}
     >
       <Logo size={logoSize} />
       {AppProperty.APP_NAME}
