9f3a7071b45577e5e31b2c1c98abfe07a72a7360
yunwi5 <1009jyk@gmail.com>
2023-05-01 00:29:47 +1200

    remove unused hook

--- next-client/hooks/ui/useEditorThemes.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import monokai from '../../assets/themes/Monokai.json';
-
-function useEditorThemes() {
-  return <div>useEditorThemes</div>;
-}
-
-export default useEditorThemes;
